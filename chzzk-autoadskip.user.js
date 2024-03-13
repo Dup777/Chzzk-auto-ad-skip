abc4c2673955fc22068388e3fd867254099dd030
Dup777 <163287300+Dup777@users.noreply.github.com>
2024-03-13 23:30:21 +0900

    Update chzzk-autoadskip.user.js

--- chzzk-autoadskip.user.js
+++ chzzk-autoadskip.user.js
@@ -1,6 +1,5 @@
 // ==UserScript==
-// @name        Chzzk auto ad skip (치지직 광고 자동 스킵)
-// @namespace   
+// @name        Chzzk auto ad skip (치지직 광고 자동 스킵) 
 // @description Chzzk auto ad skip (치지직 광고 자동 스킵)
 // @version     0.1
 // @match       https://chzzk.naver.com/*
